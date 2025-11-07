169bbcf052b8ca32d190f48f104f8538ba7b2560
Vishvanshu Chauhan <135249400+chauhanvishvanshu@users.noreply.github.com>
2025-11-07 15:45:41 +0530

    Update factorial.cpp

--- Date_14/Recursion1/factorial.cpp
+++ Date_14/Recursion1/factorial.cpp
@@ -1,18 +1,8 @@
-#include <iostream>
-using namespace std;
 
 int factorial(int n)
 {
-
-    // Calculating factorial of number
     if (n == 0 || n == 1)
         return 1;
     return n * factorial(n - 1);
 }
 
-int main()
-{
-    int num = 5;
-    cout << factorial(num) << endl;
-    return 0;
-}
